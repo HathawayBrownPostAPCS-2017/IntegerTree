c9bc347560e77a5cd0faf15ced3bc94aae39f2b2
mbuescher <mbuescher@hb.edu>
2017-10-24 19:47:06 -0400

    Updated files 10/24/2017

--- IntegerTree.java
+++ IntegerTree.java
@@ -7,6 +7,7 @@ public class IntegerTree
 private IntegerTreeNode root;  // holds the root of this BST
 private boolean messagesOn;    // whether to write messages while traversing
 
+/* -----------------------  Constructor  ------------------------------  */
 // Constructor: creates an empty BST.
 public IntegerTree()
 {
@@ -14,9 +15,48 @@ public IntegerTree()
  messagesOn = false;
 }
 
+
+/* ----------------------  Helper Methods  ----------------------------  */
+
 /** Turn on or or the message printing  */
 public void setMessages (boolean msg)
 {  messagesOn = msg;  }
+
+
+
+/** returns the tree as a String.
+ * Order is dependent on the ordering scheme of toString (IntegerTreeNode)
+ * 
+ * @return the Binary Search Tree as a String, including [brackets] and ,commas,
+ */
+public String toString()
+{
+	String str = toString(root);
+	if (str.endsWith(", "))
+	{
+		str = str.substring(0, str.length() - 2);
+	}
+	return "[" + str + "]"; 
+}
+
+// Returns a subtree as a String in (infix) order
+// You can change the order by changing the recursive calls
+private String toString (IntegerTreeNode node)
+{
+	if (node == null)  { return ""; } 
+	else 
+	{
+		return toString(node.getLeft()) +
+				node.getValue() + ", " +
+				toString(node.getRight());
+	}
+}
+
+
+/* --------------------------------------------------------------------  */
+/*  Non-recursive, top-level methods that call their respective          */
+/*    recursive versions (found below)                                   */
+/* --------------------------------------------------------------------  */
 
 /** Searches for the Integer value in the binary search tree.
  *  
@@ -36,12 +76,34 @@ public boolean contains(Integer value)
 public boolean add (Integer value)
 {
 	if (contains(value))
-		   return false;
+	{  
+		if (messagesOn)  System.out.println(value.intValue() + " is already in the tree.");
+		return false;
+	}
+	if (messagesOn)  System.out.println("Adding " + value.intValue() + " to the tree.");
 	root = add(root, value);
 	return true;
 }
 
 
+/** Removes value from this BST.  Returns true if value has been
+ *  found and removed; otherwise returns false. 
+ *  
+ *   @param the Integer to remove 
+ *   @return whether the Integer value was removed from the tree
+ */
+public boolean remove(Integer value)
+{
+  if (!contains(value))
+    return false;
+  root = remove(root, value);
+  return true;
+}
+
+
+/* --------------------------------------------------------------------  */
+/*  Recursive, private methods that navigate the Binary Search Tree      */
+/* --------------------------------------------------------------------  */
 // Recursive method to search for the Integer value
 private boolean contains (IntegerTreeNode node, Integer value)
 {
@@ -67,27 +129,40 @@ private boolean contains (IntegerTreeNode node, Integer value)
 // Precondition: the tree rooted at node does not contain the Integer value
 private IntegerTreeNode add(IntegerTreeNode node, Integer value)
 {
-	return null;
-}
-
-
-/** returns the tree as a String.
- * Order is dependent on the ordering scheme of toString (IntegerTreeNode)
- */
-public String toString()
-{
-	return "[" + toString(root) + "]"; 
-}
-
-// Returns a subtree as a String in ______ order
-private String toString (IntegerTreeNode node)
-{
-	if (node == null)  { return ""; } 
-	else 
+	if (node == null)
 	{
-		return node.getValue() + " ";
+		if (messagesOn)  { System.out.println("Found null; inserting."); }
+		node = new IntegerTreeNode(value);
 	}
+	else
+	{
+		int diff = value.compareTo(node.getValue());
+		if (diff > 0)
+		{
+			if (messagesOn)  { System.out.println("Bigger than " + node.getValue() + ", inserting on the right."); }
+			node.setRight(add (node.getRight(), value));
+		}
+		else  // diff < 0
+		{
+			if (messagesOn)  { System.out.println("Smaller than " + node.getValue() + ", inserting on the left."); }
+			node.setLeft(add (node.getLeft(), value));
+		}
+	}
+	return node;
 }
+
+
+//Adds value to this BST, 
+//Returns the root of the new tree
+//
+//Precondition: the tree rooted at node does not contain the Integer value
+private IntegerTreeNode remove(IntegerTreeNode node, Integer value)
+{
+	// this is not the correct code.  It's a placeholder so that it compiles.
+	return node;       
+}
+
+
 
 
 } // End of IntegerTree
--- IntegerTreeTest.java
+++ IntegerTreeTest.java
@@ -12,18 +12,37 @@ public class IntegerTreeTest {
 		IntegerTree myTree = new IntegerTree();
 		myTree.setMessages(true);
 		
-		System.out.print("Enter an integer to add to the tree (0 to quit): ");
-		int n = keyboard.nextInt();
+		System.out.print("Enter the tree yourself [0] or at random [1]:");
+		int choice = keyboard.nextInt();
 		
-		while (n != 0)
+		if (choice == 0)    // enter numbers into the tree
 		{
-			myTree.add(new Integer(n));
-			
 			System.out.print("Enter an integer to add to the tree (0 to quit): ");
-			n = keyboard.nextInt();
+			int n = keyboard.nextInt();
+			
+			while (n != 0)
+			{
+				myTree.add(new Integer(n));
+				System.out.println(myTree);
+				
+				System.out.print("Enter an integer to add to the tree (0 to quit): ");
+				n = keyboard.nextInt();
+			}
+		}
+		else     // make the tree at random
+		{
+			System.out.print("How many integers to attempt to add?  ");
+			int count = keyboard.nextInt();
+			System.out.print("Maximum possible integer:  ");
+			int max = keyboard.nextInt();
+			
+			for (int i = 1; i <= count; i++)
+			{
+				myTree.add(new Integer((int)(Math.random() * max) + 1));
+			}
 		}
 		
-		// System.out.println("\n\n" + myTree);
+		System.out.println("\n\nYour final tree is: \n" + myTree);
 		keyboard.close();
 	}
 	
